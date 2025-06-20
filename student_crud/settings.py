f79d18a6b31aedf8755b0bc7f0f659b3c8f89a13
Sheza02 <aslamsheza3@example.com>
2025-06-20 14:43:06 +0530

    coerg

--- student_crud/settings.py
+++ student_crud/settings.py
@@ -11,11 +11,14 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -25,7 +28,8 @@ SECRET_KEY = 'django-insecure-jv#z0w+=3-gngvhe)t9o=5$!od&8adkrn!#7m)5-a5bs_ul4a1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',".onrender.com"]
+
 
 
 # Application definition
